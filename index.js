9d7690f355ce3f005ff67fda50b8aedd30a6db6a
Luke Wurl <lwurl@nd.edu>
2018-05-15 15:09:26 -0400

    Add age field

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const handleSubmit = function(ev) {
   const users = document.querySelector('#users')
   const f = ev.target
   const userName = f.userName.value
-  users.innerHTML += '<p>' + userName + '</p>'
+  const age = f.age.value
+  users.innerHTML += '<p>' + userName + ", " + age + '</p>'
 
   f.userName.value = ''
+  f.age.value = ''
 }
 
 form.addEventListener('submit', handleSubmit)
