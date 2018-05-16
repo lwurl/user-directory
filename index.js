53cb2f3f0d13563af091b513ac9b399c179d183e
Luke Wurl <lwurl@nd.edu>
2018-05-16 15:03:09 -0400

    Use object with form

--- index.js
+++ index.js
@@ -1,45 +1,44 @@
 const form = document.querySelector('#userForm')
 
-const renderColor = function(favoriteColor){
-    const colorDiv = document.createElement('div')
-    colorDiv.style.backgroundColor = favoriteColor
-    colorDiv.style.width = '6rem'
-    colorDiv.style.height = '3rem'
-    return colorDiv
+function renderColor(color) {
+  const colorDiv = document.createElement('div')
+  colorDiv.style.backgroundColor = color
+  colorDiv.style.width = '6rem'
+  colorDiv.style.height = '3rem'
+  return colorDiv
 }
 
-const renderListItem = function(text, isColor, favoriteColor){
-    const item = document.createElement('li')
-    item.textContent = text
-    if (isColor){
-        item.appendChild(renderColor(favoriteColor))
-    }
-    return item
-}
-
-const renderList = function(userName, age, favoriteColor){
-    // Create list
-    const list = document.createElement('ul')
-
-    // Create list items
-    list.appendChild(renderListItem(`Name: ${userName}`, false, null))
-
-    list.appendChild(renderListItem(`Age: ${age}`, false, null))
-
-    list.appendChild(renderListItem('Favorite Color: ', true, favoriteColor))
-
-    return list
+function renderListItem(label, value) {
+  const item = document.createElement('li')
+  item.textContent = `${label}: `
+  try {
+    item.appendChild(value)
+  } catch(e) {
+    item.textContent += value
+  }
+  return item
 }
 
 const handleSubmit = function(ev) {
   ev.preventDefault()
-  const users = document.querySelector('#users')
   const f = ev.target
-  const userName = f.userName.value
-  const age = f.age.value
-  const favoriteColor = f.favoriteColor.value
+  const user = {
+    userName: f.userName.value,
+    age: f.age.value,
+    favoriteColor: renderColor(f.favoriteColor.value),
+  }
 
-  users.appendChild(renderList(userName, age, favoriteColor))
+  const list = document.createElement('ul')
+
+  const labels = Object.keys(user)
+  labels.forEach(function(label) {
+    const item = renderListItem(label, user[label])
+    list.appendChild(item)
+  })
+
+
+  const users = document.querySelector('#users')
+  users.appendChild(list)
 
   f.reset()
   f.userName.focus()
